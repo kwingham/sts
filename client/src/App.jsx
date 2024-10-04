24a7d97f44895082321769a14184b237db6a8c00
kwingham <kyle_wingham@outlook.com>
2024-10-04 12:44:11 +0100

    added logo picture and adjusted styling

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,10 +2,18 @@ import { Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import SubmitReview from "./pages/SubmitReview";
 import "./styles/App.css";
+import logo from "./assets/logo.webp"; // Update the path as necessary
 
 const App = () => {
   return (
     <div className="container">
+      <header className="App-header">
+        <img
+          src={logo}
+          className="logo-top-right"
+          alt="Swap Travel Stories Logo"
+        />
+      </header>
       <Routes>
         {/* Home Route */}
         <Route path="/" element={<LandingPage />} />
